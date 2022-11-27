bb57080ccace9a149bebf18a934824ea09316292
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2022-11-27 12:42:04 +0100

    pagination

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{'app.name', 'AppRiera'}}</title>
+
+    </head>
+    <body>
+            <h1>Bienvenue dans le blog</h1>
+            <p>c'est un test</p>
+    </body>
+</html>
