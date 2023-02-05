12960e96de3eb911d8611d883fe293c0134c2d98
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-02-05 18:00:19 +0100

    ajout de l'api news
    news api

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-       <h1>Accueil</h1>
-       <p>c'est la page d'accueil</p>
+    <h1>Page d'accueil</h1>
+       <script>
+        const API_KEY = 'bfbc1feae891427e9afc9f8bc24769ff';
+        const API_URL = 'https://newsapi.org/v2';
+
+        fetch(`${API_URL}/top-headlines?country=fr&apiKey=${API_KEY}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+
+        fetch(`${API_URL}/everything?q=bitcoin&apiKey=${API_KEY}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error));
+
+        </script>
 @endsection()
 
